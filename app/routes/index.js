1576ced5f50eb5d2f1924e58654f1421406bfb0d
Pawel <anandas.tear@gmail.com>
2016-02-05 11:44:27 +0000

    add usage info

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,14 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
 module.exports = function (app, passport) {
 
+	// move to controllers
 	String.prototype.replaceAll = function (find, replace) {
 	    var str = this;
 	    return str.replace(new RegExp(find, 'g'), replace);
 	};
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
 
 	function getNatural(date) {
 		var naturalMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -33,67 +25,22 @@ module.exports = function (app, passport) {
 		};	
 	}
 	
-	var clickHandler = new ClickHandler();
-
-	// app.route('/')
-	// 	.get(isLoggedIn, function (req, res) {
-	// 		res.sendFile(path + '/public/index.html');
-	// 	});
-	
 	app.route('/').get(function(req, res) {
-		res.send('Hi guy! Send date via url, f.ex.:<br>' +
-		'https://clementinedates.herokuapp.com/December%2015,%202015<br>' +
-		'https://clementinedates.herokuapp.com/1450137600');
+		res.sendFile(path + '/public/index.html');
 	});
 	
 	app.route('/*').get(function(req, res) {
 		var query = req.path.slice(1);
-		var date; 
+		var date, seconds; 
 		
 		if (/^\d+$/.test(query)) {
-			var seconds = query * 1000
+			seconds = query * 1000;
 			date = new Date(seconds);
 		} else {
-			var seconds = Date.parse(query.replaceAll('%20', ' '));
+			seconds = Date.parse(query.replaceAll('%20', ' '));
 			date = new Date(seconds);
 		}
 		
 		res.send(JSON.stringify(getTimeObject(date)));
 	});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
-		
 };
